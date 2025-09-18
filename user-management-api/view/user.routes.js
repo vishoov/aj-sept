f40277a9b1813a75b1ead7e6c0a626df289d8520
Vishoo <vverma971@gmail.com>
2025-09-18 21:49:11 +0530

    CRUD

--- user-management-api/view/user.routes.js
+++ user-management-api/view/user.routes.js
@@ -173,6 +173,116 @@ router.get("/ageFilter2", async (req, res)=>{
     }
 })
 
+router.get("/cityAge", async (req, res)=>{
+    try{
+        const { city, age } = req.query;
+
+        const users = await User.find({
+            "address.city":city,
+            age:{
+                $gte:parseInt(age)
+            }
+        })
+
+        res.status(200).json({
+            users
+        })
+
+
+    }
+    catch(err){
+        return res.status(500).json({message: err.message, status:"Failed"})
+    }
+})
+
+router.get('/random', async (req, res)=>{
+    try{
+        const user = await User.find({
+            role:'admin',
+            age:{
+                $gt:20
+            },
+            phone:{
+                $exists:true
+            }
+        })
+
+        return res.status(200).json({user, message:"Users retrieved successfully", status:"Success"})
+    }catch(err){
+        return res.status(500).json({message: err.message, status:"Failed"})
+    }
+})
+
+
+router.put('/update/:id', async (req, res)=>{
+    try{
+        const id = req.params.id;
+        const updatedData = req.body;
+
+        const user = await User.updateOne(
+            {
+                _id:id
+            },
+            {
+                $set: updatedData
+            }
+        )
+        res.status(200).json({
+            user
+        })
+    }
+    catch(err){
+        return res.status(500).json({message: err.message, status:"Failed"})
+    }
+})
+
+router.put("/updatePassword/:id", async (req, res)=>{
+    try{
+        const id = req.params.id;
+        const password = req.body.password;
+
+        const user = await User.findOneAndUpdate(
+            {
+                _id:id
+            },
+            {
+                $set:{
+                    password:password
+                }
+            },
+            {
+                runValidators:true,
+                new:true
+            }
+        )
+
+        return res.status(200).json({user, message:"Password updated successfully", status:"Success"})
+
+    }
+    catch(err){
+        return res.status(500).json({message: err.message, status:"Failed"})
+    }
+} )
+
+
+router.delete('/delete/:id', async (req, res)=>{
+    try{
+        const id = req.params.id;
+
+        const user = await User.deleteOne(
+            {
+                _id:id
+            }
+        )
+
+        return res.status(200).json({
+            user
+        })
+    }
+    catch(err){
+        return res.status(500).json({message: err.message, status:"Failed"})
+    }
+})
 
 module.exports = router;
 
